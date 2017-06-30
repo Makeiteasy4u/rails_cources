714164ec19cf4e06046c858c820af25758327617
Makeiteasy4u <tamer2555@gmail.com>
2017-06-29 17:26:05 -0700

    create new template, routes, controller_method action

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
-	
+	validates :title, presence: true, length: {minimum: 5, maximum: 20}
+	validates :description, presence: true, length: {minimum: 5, maximum: 300}
 end	
 	
