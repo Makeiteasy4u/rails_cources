17c541346810e6b6d2ab82fec9c7ddaf2074a632
Makeiteasy4u <tamer2555@gmail.com>
2017-06-29 17:31:43 -0700

    generated articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629212340) do
+ActiveRecord::Schema.define(version: 20170630002759) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
